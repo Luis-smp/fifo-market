f415921c7777e203e60d30b00921105768599a68
Luis-smp <luisotaviocerqueirasampaio@gmail.com>
2026-07-01 11:49:01 -0300

    Fix: Adicao de contador na pagina produto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,61 @@
-const modalProduto = document.querySelector('#modalProduto');
-const fecharModal = document.querySelector('#fecharModal');
-const fundoModal = document.querySelector('.fundo-modal');
-const cardsProdutos = document.querySelectorAll('.card-produto');
+const numero = document.getElementById('number')
+const btnDiminuir = document.getElementById('subtracao')
+const btnAumentar = document.getElementById('adicao')
+const btnAumentar1 = document.getElementById('adicao1')
+const btnAumentar2 = document.getElementById('adicao2')
+const btnAumentar3 = document.getElementById('adicao3')
+const precoProduto = document.getElementById('preco')
 
-cardsProdutos.forEach((card) => {
-    card.addEventListener('click', () => {
-        modalProduto.classList.add('ativo');
-    });
-});
+let contador = 0
+let preco = 2.99
 
-fecharModal.addEventListener('click', () => {
-    modalProduto.classList.remove('ativo');
-});
 
-fundoModal.addEventListener('click', () => {
-    modalProduto.classList.remove('ativo');
-});
+
+btnAumentar.addEventListener('click', () => {
+    contador++
+    preco = preco + 2.99
+    numero.textContent = contador
+    precoProduto.innerHTML = ` <p>R$ ${preco.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} <p> `
+})
+
+
+btnDiminuir.addEventListener('click', () => {
+
+
+    contador--
+    
+
+    if (contador < 0) {
+        alert('Não pode valores negativos')
+        contador = 0
+
+    } else {
+        preco = preco - 2.99
+        numero.textContent = contador
+        precoProduto.innerHTML = ` <p>R$ ${preco.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} <p> `
+
+    }
+
+})
+
+btnAumentar1.addEventListener('click', () => {
+    contador = 1
+    numero.textContent = contador
+})
+btnAumentar2.addEventListener('click', () => {
+    contador = 2
+    numero.textContent = contador
+})
+btnAumentar3.addEventListener('click', () => {
+    contador = 3
+    numero.textContent = contador
+})
+
+
+
+
+
+
+
+
+
